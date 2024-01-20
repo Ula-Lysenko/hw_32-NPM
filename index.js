751d90d866e97bc921329c2eb8781c4639c36fc2
Ula-Lysenko <highspirit@ukr.net>
2024-01-20 23:30:03 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function sum(a, b) {
+    return a + b;
+}
+
+const result = sum(1, 2);
+console.log(result);
+
+function sub(a, b) {
+    return a - b;
+}
+
+const result2 = sub(11, 2);
+console.log(result2);
+
+function multi(a, b) {
+    return a * b;
+}
+
+const result3 = multi(10, 2);
+console.log(result3);
